99cef40f79a809a0573e1f0a999409f21dd2634c
Kurt Baux <ktripodibaux@gmail.com>
2022-06-10 18:31:37 -0400

    commit tree df3150dacd209655e5727cffe67350d52bf8c05d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const input = e.target.searchByID.value;
+        if (input == 1 || input == 2 || input == 3) {
+            const title = document.getElementById('title');
+            const desc = document.getElementById('desc');
+            fetchResource(`http://localhost:3000/movies/${input}`).then(function (data)  {title.textContent = data.title; desc.textContent = data.summary; console.log()});
+
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+fetchResource('http://localhost:3000/movies').then(data => console.log(data));
+
+function fetchResource(url) {
+    return fetch(url).then(res => res.json());
+}
